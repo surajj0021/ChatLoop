d95207b7986d1cb35735dbf4ba8c249f7f0848fe
surajj0021 <surajjunare87@gmail.com>
2026-09-23 21:37:06 +0530

    created index.js

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+ import express from "express"
+ import dotenv from "dotenv"
+dotenv.config()
+
+const port =process.env.PORT || 5000
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("Server is running");
+})
+
+app.listen(port,()=>{
+    console.log("🟢  server Started")
+})
+
+
+
+
